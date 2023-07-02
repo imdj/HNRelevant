adb5a2026f494b38f88323c4a0c428600ac46286
imdj <69906094+imdj@users.noreply.github.com>
2023-07-02 18:07:42 +0300

    pump version to 0.1.6

--- HNRelevant.user.js
+++ HNRelevant.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         HNRelevant
-// @version      0.1.5
+// @version      0.1.6
 // @description  Related submissions sidebar for Hacker News
 // @author       imdj
 // @match        https://news.ycombinator.com/item*
