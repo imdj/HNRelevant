2e52c100877b3d263f99d3abfa00e4fb365192ed
imdj <os@imadij.com>
2024-01-20 15:05:25 +0200

    Release version 0.1.8

--- HNRelevant.user.js
+++ HNRelevant.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         HNRelevant
-// @version      0.1.7
+// @version      0.1.8
 // @description  Shows relevant submissions on Hacker News
 // @author       imdj
 // @match        https://news.ycombinator.com/item*
